1d2b599f98f953f46cc5ca49a40481f030405156
Gaiapp <arkopharmaexport@gmail.com>
2026-06-30 22:26:31 +0200

    Load hand drawn overrides

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
+import './handdrawn.css'
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
